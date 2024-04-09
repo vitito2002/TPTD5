b6d05d560de5a0c4dae236fec9eb858c37fce956
miladesoja <lizzwurzel@gmail.com>
2024-04-09 12:27:04 -0300

    calcular error funciona!!

--- auxiliares.py
+++ auxiliares.py
@@ -1,5 +1,8 @@
 def calcular_error(inicio, fin, punto):
     #funcion lineal y=mx+b
+    if punto["x"][0] < inicio[0] or punto["x"][0] > fin[0]:
+        raise ValueError("La coordenada x del punto está fuera del dominio de la línea")
+    
     pendiente = (fin[1] - inicio[1]) / (fin[0] - inicio[0])
     ordenada = inicio[1] - pendiente * inicio[0]
 
@@ -10,3 +13,10 @@ def calcular_error(inicio, fin, punto):
     error = abs(punto["y"][0] - distancia_a_punto)
 
     return error
+
+inicio = (1, 1)
+fin = (3, 4)
+punto = { "x": [2.0], "y": [1.0] }
+
+error = calcular_error(inicio, fin, punto)
+print("Error:", error)
