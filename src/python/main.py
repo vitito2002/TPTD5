f4c65596e5f29543bb5ddeb88a3494de3185874f
miladesoja <lizzwurzel@gmail.com>
2024-04-13 13:30:22 -0300

    resaltador amarilla faber castell

--- src/python/main.py
+++ src/python/main.py
@@ -1,85 +1,125 @@
 import json
 import numpy as np
-import os  
-"""
-# from algoritmos import FB, calcular_error 
-# Importa las funciones de fuerzabruta.py
-"""
-def calcular_error(inicio, fin, punto):
-    #funcion lineal y=mx+b
-    if punto["x"][0] < inicio[0] or punto["x"][0] > fin[0]:
-        raise ValueError("La coordenada x del punto está fuera del dominio de la línea")
-    
-    pendiente = (fin[1] - inicio[1]) / (fin[0] - inicio[0])
-    ordenada = inicio[1] - pendiente * inicio[0]
-
-    # Calculate the y-coordinate corresponding to the x-coordinate of point c on the line
-    distancia_a_punto = pendiente * punto["x"][0] + ordenada
-
-    # Calculate the error in terms of y-coordinate
-    error = abs(punto["y"][0] - distancia_a_punto)
-
-    return error
-
-def FB(N, M, K, puntos, res=None):
-    # Función recursiva de fuerza bruta para buscar la solución óptima
-    if res is None:
-        res = []  # Inicializa una nueva lista vacía si res no se proporciona
-
-    # Caso base:
-    if N > len(puntos["x"]) or M > len(puntos["y"]) or M < 0 or (K > 0 and N == len(puntos["x"])) or (K == 0 and N < len(puntos["x"])):
-        return []  # Devuelve una lista vacía si no se cumple alguna de estas condiciones
-    elif K == 0 and N == len(puntos["x"]):
-        return res  # Devuelve el resultado final si K es 0 y N es igual al número de puntos
-
-    # Caso inductivo:
-    mejor_error = float('inf')  # Inicializa el mejor error con un valor infinito
-    mejor_solucion = None
-
-    for i in range(1, N + 1):
-        for j in range(-M, M + 1):
-            nueva_solucion = res[:] + [(i, j)]
-            error_total = sum(calcular_error(puntos["x"][nueva_solucion[k][0]], puntos["x"][nueva_solucion[k+1][0]], puntos) for k in range(len(nueva_solucion) - 1))
-            
-            if error_total < mejor_error:
-                mejor_error = error_total
-                mejor_solucion = nueva_solucion
-
-    # Llama recursivamente a la función con las nuevas coordenadas y K-1
-    return FB(N, M, K - 1, puntos, mejor_solucion)
-
+import time
 
 BIG_NUMBER = 1e10 # Revisar si es necesario.
 
 def main():
+
 	# Ejemplo para leer una instancia con json
 	instance_name = "titanium.json"
 	filename = "data/" + instance_name
-	 # Verificar si el archivo existe
-	if not os.path.exists(filename):
-		print(f"Error: No se encontró el archivo {filename}")
-		return
-	 # Verificar si el archivo existe
-	
 	with open(filename) as f:
 		instance = json.load(f)
 	
-	K = 5 #gato encerrado cantidad de breakpoints
+	K = instance["n"]
 	m = 6
 	n = 6
-	Nodo = instance["n"]
-	puntos = instance # diccionario
+	N = 5
+	
 	# Ejemplo para definir una grilla de m x n.
+ 
+	# np.linspace genera:
+ 		# M puntos equiespaciados en grid_x desde el valor minimo hasta el maximo de instance["x"]
+		# N puntos equiespaciados en grid_y desde el ...
+
 	grid_x = np.linspace(min(instance["x"]), max(instance["x"]), num=m, endpoint=True)
 	grid_y = np.linspace(min(instance["y"]), max(instance["y"]), num=n, endpoint=True)
+	x = instance['x']
+	y = instance['y']
+
+	sol= []
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-	# TODO: aca se deberia ejecutar el algoritmo.
-	result = FB(n, m, K, puntos, res=None)
+	def calcular_error(sol, x, y):
+
+		error_funcion = 0
+		
+		# para iterar los breakpoints
+		for i, (primer, ultimo) in enumerate(zip(sol[:-1], sol[1:])):
+
+			# me fijo si cada punto del json esta en el dominio entre breakpoints
+			x_en_rango = [punto for punto in x if primer[0] <= punto <= ultimo[0]]
+			
+			# guardo el y de cada uno de los puntos de x_en_rango para despues poder calcular el error 
+			primer_y, ultimo_y = [x.index(x_en_rango[0]), x.index(x_en_rango[-1]) + 1]
+			y_en_rango = y[primer_y:ultimo_y]
+
+			# armo tramo entre los dos breakpoints para despues distancia a punto
+			pendiente = (ultimo[1] - primer[1]) / (ultimo[0] - primer[0])
+			ordenada_al_origen = primer[1] - pendiente * primer[0]
+
+			# defino funcion y calculo el error para cada punto
+			diferencia = [
+				abs(pendiente * punto + ordenada_al_origen - y_en_rango[j]) 
+				for j, punto in enumerate(x_en_rango)
+			]
+
+			# sumo todos los errores :)
+			error_funcion += np.sum(diferencia)
+		
+		return error_funcion
 
 
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	# FUNCION FUERZA BRUTA
+	def FB(grid_x, grid_y, x, y, _k, sol):
+
+		if (len(grid_x) < _k - (len(sol))):
+			return {'error':1e10}
+
+         # CASO BASE: # alcance los breakpoints pedidos
+		elif len(sol) == _k:
+            # DEVUELVO combinacion de breakpoints con error correspondiente #¿QUIZAS ARMAR UN DICC COMO VARIABLE PRIMERO?
+			return {'error':calcular_error(sol, x, y), 'puntos':sol.copy()}
+        
+		else:
+            # armo una solucion con error altisimo para despues ir reemplazando
+			mejores_breakpoints = {'error':9999999999999999999999}
+            
+			if (_k - (len(sol)) == 1):
+				grid_x = [grid_x[-1]]   
+
+			# itero sobre filas
+			for i in grid_y:
+                # sumo breakpoint
+				sol.append((grid_x[0],i))
+                # recorto valores de gridX y hago recursion
+				recursion = FB(grid_x[1:],grid_y,x,y,_k,sol)
+
+                # si error de la parcial es menor la reemplazo
+				if recursion['error'] < mejores_breakpoints['error']:
+					mejores_breakpoints = recursion
+
+                # saco el ultimo elemento asi pruebo con otras opciones
+				sol.pop()
+
+			if len(sol)>0 :
+                # recorto valores de gridX y hago recursion
+				recursion = FB(grid_x[1:],grid_y,x,y,_k,sol)
+
+                # si error de la parcial es menor la reemplazo
+			if recursion['error'] < mejores_breakpoints['error']:
+				mejores_breakpoints = recursion
+
+			return mejores_breakpoints
+
+
+	
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	comienzo_timer_FB = time.time()
+	print('Fuerza Bruta ',FB(grid_x,grid_y,x,y,5,sol))
+	fin_timer_FB = time.time()
+	timer_FB = fin_timer_FB - comienzo_timer_FB
+	print('Tiempo FB:',timer_FB)
+	
 	best = {}
-	best['sol'] = [None]*(Nodo+1)
+	best['sol'] = [None]*(N+1)
 	best['obj'] = BIG_NUMBER
 	
 	# Posible ejemplo (para la instancia titanium) de formato de solucion, y como exportarlo a JSON.
@@ -102,7 +142,6 @@ def main():
 	# Se guarda el archivo en formato JSON
 	with open('solution_' + instance_name, 'w') as f:
 		json.dump(solution, f)
-	print("Resultado de la función FB:", result)
-
+	
 if __name__ == "__main__":
 	main()
